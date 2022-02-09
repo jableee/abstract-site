49e6e189fd18e4ef4c5e13a5b6184b19603b9af7
jableee <96569903+jableee@users.noreply.github.com>
2022-02-10 01:14:33 +0900

    순한맛 index.js
    순한맛 index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,77 @@
-class Site {}
+class Site {
+    constructor() {
+        this.boards = [];
+    }
 
-class Board {}
+    addBoard(board) {
+        const findBoard = this.boards.findIndex((i) => i.name === board.name);
+        if (findBoard === -1) {
+            board.addedboard = true;
+            return this.boards.push(board);
+        } else {
+            throw new Error();
+        }
+    }
 
-class Article {}
+    findBoardByName(boardname) {
+        const findboard = this.boards.findIndex((i) => i.name === boardname);
+        return this.boards[findboard]; //
+    }
+}
 
-class Comment {}
+class Board {
+    constructor(name) {
+        if (!name) {
+            throw new Error();
+        }
+        this.name = name;
+        this.addedboard = false;
+        this.articles = [];
+    }
+
+    publish(article) {
+        // Site에 추가된 Board에만 article추가 할수 있게 하기
+        if (!this.addedboard) throw new Error();
+
+        article.id = `${this.name}-${Math.random()}`;
+        article.createdDate = new Date().toISOString();
+        article.comment = [];
+        this.articles.push(article);
+    }
+
+    getAllArticles() {
+        return this.articles;
+    }
+}
+
+class Article {
+    constructor(article) {
+        const { subject, content, author } = article;
+        if (!subject || !content || !author) throw new Error();
+
+        this.subject = subject;
+        this.content = content;
+        this.author = author;
+    }
+
+    reply(comment) {
+        comment.createdDate = new Date().toISOString();
+        this.comment.push(comment);
+    }
+    getAllComments() {
+        return this.comment;
+    }
+}
+
+class Comment {
+    constructor(comment) {
+        const { content, author } = comment;
+        if (!content || !author) throw new Error();
+
+        this.content = content;
+        this.author = author;
+    }
+}
 
 module.exports = {
     Site,
